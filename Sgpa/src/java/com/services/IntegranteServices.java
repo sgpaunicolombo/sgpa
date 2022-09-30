2a55dd789becf75fec197ab9e32e9dfc72c64ee8
julio <jcmm80@gmail.com>
2022-09-30 17:10:35 -0500

    Cambios profe julio en gestion propuesta
    Se puso metodos para proyectoaula para encontrar matricula y periodo

--- Sgpa/src/java/com/services/IntegranteServices.java
+++ Sgpa/src/java/com/services/IntegranteServices.java
@@ -6,8 +6,10 @@ package com.services;
 
 import com.dao.ImplDao;
 import com.entity.Integrante;
+import com.entity.Matricula;
 import com.entity.Periodo;
 import com.entity.ProgramaAcademico;
+import com.entity.Proyecto_Aula;
 import com.implDao.IIntegrante;
 import java.io.Serializable;
 import java.util.LinkedList;
@@ -36,5 +38,43 @@ public class IntegranteServices extends ImplDao<Integrante, Long> implements IIn
              ex.printStackTrace();
          } 
          return integrantes;
+    }
+   
+    public List<Integrante> obtenerIntegrantesProyecto(Proyecto_Aula pr){
+         List<Integrante> integrantes=new LinkedList();
+         try {             
+             EntityManager em =getEntityManagger();
+             em.getTransaction().begin();               
+             String q="select i from Integrante i where i.proyecto.id = ?1";        
+             System.out.println(" Consulta: "+q);
+             Query qu=em.createQuery(q)
+                     .setParameter(1, pr.getId());
+             integrantes=qu.getResultList();            
+             
+             em.close();             
+         }catch(Exception ex){
+             ex.printStackTrace();
+         } 
+         return integrantes;
+    }
+   
+   
+   
+   
+    public Integrante obtenerIntegranteXMatricula(Matricula m){
+        EntityManager em =ImplDao.getEntityManagger();
+        Integrante inte=new Integrante();
+        em.getTransaction().begin();        
+        try{
+        String q="select i from Integrante i where i.matricula.id = ?1";        
+        Query qu=em.createQuery(q)
+                .setParameter(1, m.getId());
+        inte=(Integrante)qu.getSingleResult();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            em.close();        
+        }        
+        return  inte;
     }
 }
